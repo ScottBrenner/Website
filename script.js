0033769af6b0242bc6de0529742b1a5008290248
Scott Brenner <scottstimo@gmail.com>
2015-08-23 22:55:40 -0700

    Refresh
    Added JQuery and material design

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$(".code").click(function() {
+		window.location="https://github.com/scottbrenner";
+	});
+
+	$(".photography").click(function() {
+		window.location="https://500px.com/ScottBrenner";
+	});
+
+	$(".resume").click(function() {
+		window.location.href="resume.html";
+	});
+});
