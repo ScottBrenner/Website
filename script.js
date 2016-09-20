bd1330714085ad15f204a30f9a2e227c56dd8f7a
Scott Brenner <scottstimo@gmail.com>
2016-09-20 12:45:25 -0700

    Beautify

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	$("img").fadeIn("slow");
-	$(".name").fadeIn("slow");
-	$(".projects").fadeIn("slow");
-	$(".photography").fadeIn("slow");
-	$(".resume").fadeIn("slow");
+    $("img").fadeIn("slow");
+    $(".name").fadeIn("slow");
+    $(".projects").fadeIn("slow");
+    $(".photography").fadeIn("slow");
+    $(".resume").fadeIn("slow");
 
-	$(".projects").click(function() {
-		window.location.href="projects.html";
-	});
+    $(".projects").click(function() {
+        window.location.href = "projects.html";
+    });
 
-	$(".photography").click(function() {
-		window.location="https://500px.com/ScottBrenner";
-	});
+    $(".photography").click(function() {
+        window.location = "https://500px.com/ScottBrenner";
+    });
 
-	$(".resume").click(function() {
-		window.location.href="resume.html";
-	});
+    $(".resume").click(function() {
+        window.location.href = "resume.html";
+    });
 });
