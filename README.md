41aa301905b48aab1f609a2a4de6a358a1e78806
Scott Brenner <scottstimo@gmail.com>
2016-12-26 13:47:56 -0800

    Index revamp!

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Website files! 
+Files for my website!
 
 http://scottbrenner.me/
 
-Created using HTML, CSS, JQuery and Javascript. Resources used from Google'ss Material Design Lite (http://www.getmdl.io/).
+Created using HTML, CSS, JQuery and Javascript. Resources used from Google's Material Design Lite library (http://www.getmdl.io/) and Font Awesome icons (http://fontawesome.io/icons/).
--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-    $("img").fadeIn("slow");
-    $(".name").fadeIn("slow");
-    $(".projects").fadeIn("slow");
-    $(".photography").fadeIn("slow");
-    $(".resume").fadeIn("slow");
-
-    $(".projects").click(function() {
-        window.location.href = "projects.html";
-    });
-
-    $(".photography").click(function() {
-        window.location = "https://500px.com/ScottBrenner";
-    });
-
-    $(".resume").click(function() {
-        window.location.href = "resume.html";
-    });
+    $("#info").fadeIn("slow");
+    $("#resume").fadeIn("slow");
 });
